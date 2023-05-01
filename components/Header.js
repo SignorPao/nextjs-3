9da3612b919d4db4b8c522ccf2085bc456ed4c38
Pavel Belousov <signorpao@gmail.com>
2023-05-01 19:26:37 +0300

    header...

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import "../styles/header.css";
+import { categories } from "../data";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [visible, setVisible] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-export default Header
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    console.log({ scrolled });
+    if (scrolled > 0) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
+  return (
+    <header
+      style={{
+        background: visible || showMenu ? "var(--bg-main)" : "transparent",
+        boxShadow: visible ? "2px 20px 30px var(--shadow-color)" : "none",
+      }}
+    >
+      <nav>
+        <a href="#" className="logo">
+          <i className="fa-solid fa-person-hiking"></i> Travel
+        </a>
+
+        <div
+          className="menu_btn"
+          onClick={() => setShowMenu((value) => !value)}
+        >
+          {showMenu ? (
+            <i className="fa-solid fa-xmark"></i>
+          ) : (
+            <i className="fa-solid fa-bars"></i>
+          )}
+        </div>
+
+        <ul
+          style={{
+            height: showMenu ? 250 : 0,
+          }}
+        >
+          {categories.map((category) => (
+            <li key={category.name}>
+              <a href={category.link} className="">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ data.js
@@ -0,0 +1,7 @@
+// navigation
+export const categories = [
+  { name: "home", link: "#" },
+  { name: "trending", link: "#trending" },
+  { name: "destination", link: "#destination" },
+  { name: "testimonials", link: "#testimonials" },
+];
