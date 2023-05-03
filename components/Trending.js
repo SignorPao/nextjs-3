a4723325adc19735f2370ee8346d5ac9b77b7054
Pavel Belousov <signorpao@gmail.com>
2023-05-03 18:13:08 +0300

    trends

---
+++ components/Trending.js
@@ -0,0 +1,93 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import "../styles/trending.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+import { trends } from "../data";
+
+const Trending = () => {
+  return (
+    <section id="trending">
+      <div className="heading">
+        <h2>Trending 2023</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, dolorum
+        </p>
+      </div>
+
+      <div className="wrapper">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          breakpoints={{
+            800: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1100: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          {trends.map((trend) => (
+            <SwiperSlide key={trend.id}>
+              <div className="card">
+                <div className="title">
+                  <span>
+                    <i className="fas fa-map-marker-alt"></i> {trend.location}
+                  </span>
+                  <a href="#!">{trend.name}</a>
+                </div>
+
+                <div className="img-container">
+                  <Image src={trend.img} alt="trend" width={350} height={170} />
+                </div>
+
+                <div className="text">
+                  <span>
+                    <i className="fa-solid fa-shoe-prints"></i>{" "}
+                    {trend.activities} Activities
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-compass"></i> {trend.places}{" "}
+                    Places
+                  </span>
+                  <span>
+                    <i className="fa-solid fa-calendar-days"></i> {trend.days}
+                  </span>
+                </div>
+
+                <hr />
+
+                <div className="price">
+                  <p>
+                    From ${trend.price} <span>/Person</span>
+                  </p>
+                  <a href="#!">Book Now</a>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Trending;
--- data.js
+++ data.js
@@ -12,3 +12,57 @@ export const banners = [
   "https://raw.githubusercontent.com/devat-youtuber/travel-nextjs13beta-images/main/bn2.jpg",
   "https://raw.githubusercontent.com/devat-youtuber/travel-nextjs13beta-images/main/bn3.jpg",
 ];
+
+// trends
+export const trends = [
+  {
+    id: 1,
+    name: 'thailand',
+    location: "koh tao island, thailand",
+    img: "https://img.freepik.com/free-photo/beautiful-girl-standing-viewpoint-koh-nangyuan-island-near-koh-tao-island-surat-thani-thailand_335224-1094.jpg?w=996&t=st=1683122285~exp=1683122885~hmac=7faa27dfbe6e14315787985dede6f7db0071d995942fdd7abbcfc3d150d3df98",
+    activities: 10,
+    places: 12,
+    days: '1 week',
+    price: 1.2
+  },
+  {
+    id: 2,
+    name: 'thailand',
+    location: "koh tao island, thailand",
+    img: "https://img.freepik.com/free-photo/woman-hiking-mountains-looking-map_1303-11175.jpg?w=996&t=st=1683126516~exp=1683127116~hmac=87deb04b8163276d92ba0a35efacbd4aeb3500ac2f66fbfc3475c386af330724",
+    activities: 10,
+    places: 12,
+    days: '1 week',
+    price: 1.2
+  },
+  {
+    id: 3,
+    name: 'thailand',
+    location: "koh tao island, thailand",
+    img: "https://img.freepik.com/free-photo/young-positive-tourist-holds-travel-paper-map-reads-route-trip_273609-25787.jpg?w=996&t=st=1683126652~exp=1683127252~hmac=eecbf20e247b98af368fd3c4bc04ad0cd445d8d81f282533ff7b01b11bc9bc85",
+    activities: 10,
+    places: 12,
+    days: '1 week',
+    price: 1.2
+  },
+  {
+    id: 4,
+    name: 'thailand',
+    location: "koh tao island, thailand",
+    img: "https://img.freepik.com/free-photo/two-happy-sisters-outdoors_624325-528.jpg?w=996&t=st=1683126677~exp=1683127277~hmac=b03a9ef2795e4b2dfbe241cfc7732c240f2df2f0cea3a1cd9dcbf4ed80ac769c",
+    activities: 10,
+    places: 12,
+    days: '1 week',
+    price: 1.2
+  },
+  {
+    id: 5,
+    name: 'thailand',
+    location: "koh tao island, thailand",
+    img: "https://img.freepik.com/free-photo/tourist-sitting-phu-sub-lek-viewpoint-sunset-lopburi-thailand_335224-1390.jpg?w=996&t=st=1683126723~exp=1683127323~hmac=ae81d7c618cee514124477c1f7585c137db5e52bd353b659389352c07f81636d",
+    activities: 10,
+    places: 12,
+    days: '1 week',
+    price: 1.2
+  },
+];
