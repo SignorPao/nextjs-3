79fb12517756002072d7af392b6fe50354e6f234
Pavel Belousov <signorpao@gmail.com>
2023-06-23 10:57:02 +0200

    already...

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Theme from "@/components/Theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.png" />
       </head>
       <body className={inter.className}>
+        <Theme />
         <Header />
         {children}
         <Footer />
